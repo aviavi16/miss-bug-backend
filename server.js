06b76f0d4435692ea354301fe07debda9e233c80
aviavi16 <eranis54321@gmail.com>
2024-10-29 12:14:37 +0200

    fixing the base url again

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/user', userRoutes )
 import {authRoutes} from './api/auth/auth.routes.js'
 app.use('/api/auth', authRoutes )
 
-const port = 3030
+const port = process.env.PORT || 3000;
 
 app.get('/**', (req, res) => {
     res.sendFile( path.resolve('public/index.html'))
